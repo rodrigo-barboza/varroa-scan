f9200f888525355ce239a3c96d2cf0fd735bf9ed
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-03-06 00:14:27 -0300

    atualiza o script do modelo deixando igual do colab

--- yolo/model.py
+++ yolo/model.py
@@ -1,24 +1,21 @@
 import os
 from ultralytics import YOLO
 
-# Treinamento do modelo
 model = YOLO("yolov8n.yaml")  # Arquitetura base (pode ser 'yolov8m.yaml' ou 'yolov8l.yaml' para modelos maiores)
 
 model.train(
     data=os.path.abspath("yolo-model/data.yml"),
-    epochs=50,  # Ajuste conforme necessário
+    epochs=50,
     batch=16,
-    imgsz=640,  # Ajuste conforme o tamanho das imagens do dataset
+    imgsz=320,
 )
 
-# Carrega o modelo treinado
 model = YOLO("runs/detect/train/weights/best.pt")  # Caminho para os pesos treinados
 
 print(model)
-# # Faz a inferência em uma imagem
+
 results = model(os.path.abspath("yolo-model/bee-with-mite.jpg"), save=True)
 
-# Conta a ocorrência do ácaro Varroa (assumindo que ele tem ID 1)
 contagem_varroa = sum(1 for r in results[0].boxes.cls if r == 1)
 
 print(f"Total de Varroa detectados: {contagem_varroa}")
