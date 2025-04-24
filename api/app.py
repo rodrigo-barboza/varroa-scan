5e6f8cfe0bbbd55a7dfcd640ca0f2eb1505b2182
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-04-23 22:48:25 -0300

    adiciona rota ping de teste

--- api/app.py
+++ api/app.py
@@ -8,6 +8,11 @@ CORS(app)
 
 app.register_blueprint(api, url_prefix='/api/v1')
 
+@app.route('/ping', methods=['GET'])
+def ping():
+    return 'pong'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
