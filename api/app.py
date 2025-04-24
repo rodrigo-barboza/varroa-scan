172f918a7d871ea1e1675f6d667220f29e93eef4
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-04-24 00:08:33 -0300

    remove configuração do cors da api e adiciona no nginx

--- api/app.py
+++ api/app.py
@@ -1,19 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
 from main.routes import api
 
 app = Flask(__name__)
-
-CORS(
-    app,
-    resources={
-        r"/v1/*": {
-            "origins": ["https://localhost", "http://localhost", "capacitor://localhost"],
-            "methods": ["GET", "POST", "OPTIONS"],
-            "allow_headers": ["Content-Type", "Authorization"]
-        }
-    }
-)
 
 app.register_blueprint(api, url_prefix='/v1')
 
