323110775828db826d57a09854087d6761d8a579
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2025-06-09 23:47:45 -0300

    diminui a confiança para 80%

--- api/main/routes.py
+++ api/main/routes.py
@@ -30,7 +30,7 @@ def predict():
         for image_path in image_paths:
             model.load_best_weights()
             model.predict(image_path)
-            model.filter_by_confidence(0.85)
+            model.filter_by_confidence(0.80)
             predict_info = model.get_predict_info()
 
             if predict_info:
